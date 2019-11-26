80b997d11673b5f02525ca2e6e4a6c7693da93c0
sandy-dev <subhash.mukho@gmail.com>
2019-11-26 14:48:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
+const schema = require('./schema/schema')
+const path = require('path')
+const app = express()
+
+// Allow cross-origin
+app.use(cors())
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test3')
+
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+})
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
+)
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
