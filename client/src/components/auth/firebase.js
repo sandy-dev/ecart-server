973faebcf0727ec432c32893da9091d38d54fbc9
subhash_dev <51679395+subhash-ranjan@users.noreply.github.com>
2021-06-05 20:40:43 +0530

    Update firebase.js

--- client/src/components/auth/firebase.js
+++ client/src/components/auth/firebase.js
@@ -2,8 +2,8 @@ import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyB_PKaFrnLxfAPwU19l7KE4LRLmJgM373Y",
-    authDomain: "elite-caster-236203.firebaseapp.com",
+    apiKey: "***",
+    authDomain: "***",
 }
 
 firebase.initializeApp(firebaseConfig)
